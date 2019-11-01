0fc1b343a6d5ec3a7df8744887adada239e99c33
basovart000 <basovart000@CAM-LAB-14.bgsd.k12.wa.us>
2019-11-01 07:48:54 -0700

    Commit.

--- src/PokemonBattle.java
+++ src/PokemonBattle.java
@@ -1,9 +1,35 @@
+/*
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
 
+import java.util.*;
 public class PokemonBattle {
-
+	static Scanner userInput = new Scanner (System.in);
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		battleStart();
+		damage();
 	}
 
+	public static void battleStart() {
+		System.out.println("Another trainer is issuing a challenge!");
+		System.out.println("Zebstrika appeared.");
+		System.out.println("Which Pokemon do you choose?");
+		String Pokename = userInput.nextLine();
+		System.out.println("You chose " + Pokename + "!");
+		System.out.println("It's a Pokemon battle between Zebstrika and " + Pokename + "! Go!");
+	}
+	public static void damage() {
+		System.out.println("Zebstrika used Thunderbolt!");
+		System.out.println("Trainer, what are your Arcanine's stats?");
+		System.out.print("Level:");
+		String Level = userInput.nextLine();
+		System.out.println("Attack:");
+	}
 }
