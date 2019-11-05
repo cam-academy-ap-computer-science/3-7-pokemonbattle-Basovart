78bf2f27fbd783b005a565a5ea51573822123a62
artyom <artyom@10.10.72.70>
2019-11-05 11:03:05 -0800

    DoneWithComments.

--- src/PokemonBattle.java
+++ src/PokemonBattle.java
@@ -1,19 +1,19 @@
-/*1. method named battlestart
+/*PSEUDOCODE
+ *1. method named battle start
  * 	Intro and prompt for name
  * 	user input name
  *2. method named damage
  *	ask for stats 
- *	use the damege formula 
+ *	use the damage formula 
  *3. stats table
  *	uses stats inputed in method damage
  *	input stats and use to create table 
- *.85 + (Math.random() * .15)
  */
 
 import java.util.*;
 public class PokemonBattle {
 	static Scanner userInput = new Scanner (System.in);
-	public static void main(String[] args) {
+	public static void main(String[] args) { //calls the three methods
 		String name = battleStart();
 		int newHP = damage(name);
 		statsTable(name, newHP);
@@ -21,19 +21,20 @@ public class PokemonBattle {
 	}
 		
 
-	public static String battleStart() {
+	public static String battleStart() { //method that starts the battle
 		System.out.println("Another trainer is issuing a challenge!");
 		System.out.println("Zebstrika appeared.");
 		System.out.println("Which Pokemon do you choose?");
-		String Pokename = userInput.nextLine();
+		String Pokename = userInput.nextLine(); //asks for player to input pokemon name
 		System.out.println("You chose " + Pokename + "!");
 		System.out.println("It's a Pokemon battle between Zebstrika and " + Pokename + "! Go!");
-		return Pokename;
+		System.out.println();
+		return Pokename; //returns the name to main method to use for next methods
 	}
 	public static int damage(String name) {
 		System.out.println("Zebstrika used Thunderbolt!");
 		System.out.println("Trainer, what are your " + name + "'s stats?");
-		System.out.print("Level: ");
+		System.out.print("Level: ");    //asks for input from player in all of the stats
 		int Level = userInput.nextInt();
 		System.out.print("Attack: ");
 		int Attack = userInput.nextInt();
@@ -46,14 +47,15 @@ public class PokemonBattle {
 		System.out.print("HP: ");
 		int HP = userInput.nextInt();
 		System.out.println();
-		double Modifier = (0.85 + (Math.random() * 0.15)) * STAB; 
-		int Damage = (int)((((2 * Level + 10) / (250)) + ((Attack / Defense) * Base) + 2) * Modifier);
-		System.out.print("Alakazam sustained " + Damage + " points of damage.");
-		int newHP = HP - 10;
-		System.out.println("HP, after damage, are now" + newHP);
-		return newHP;
+		double Modifier = (0.85 + (Math.random() * 0.15)) * STAB; //calculates the modifier
+		int Damage = (int)((((2 * Level + 10) / (250)) + ((Attack / Defense) * Base) + 2) * Modifier); //calculates the damage dealt
+		System.out.println(name + " sustained " + Damage + " points of damage."); 
+		int newHP = HP - 10; //calculates the new HP after attack
+		System.out.println("HP, after damage, is now " + newHP);
+		System.out.println();
+		return newHP; //returns the HP value after attack
 	}
-	public static void statsTable(String name, int newHP) {
+	public static void statsTable(String name, int newHP) { //prints the stats
 		System.out.println("Name	  " + name);
 		System.out.println("Level	  70");
 		System.out.println("------------------------------"); 
